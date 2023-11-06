b02558b8677885aa8b04fc0bce49430697a82a63
AttraqtiA <114904516+AttraqtiA@users.noreply.github.com>
2023-11-06 10:46:05 +0700

    client lengkap + bottom nav bar

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -14,95 +14,136 @@ class ClientSeeder extends Seeder
     public function run(): void
     {
         DB::table("clients")->insert([
-            'company_name' => 'Pandulima Group (JKT, SBY, BDG)',
-            'location' => '',
+            'company_name' => 'Pandulima Group',
+            'location' => 'Jakarta, Surabaya, Bandung',
             'company_logo' => 'public/images/pandulima_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Data Utama Dinamika',
-            'location' => '',
+            'location' => 'Surabaya',
             'company_logo' => 'public/images/datautama_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Pancamanunggal Wiradinamika',
-            'location' => '',
+            'location' => 'Semarang',
             'company_logo' => 'public/images/pancamanunggal_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Mitsol Teknik Indonesia',
-            'location' => '',
+            'location' => 'Surabaya',
             'company_logo' => 'public/images/mitsol_logo.png',
         ]);
         DB::table("clients")->insert([
-            'company_name' => 'PT. Neoduta Centra Hydraulic Indonesia ',
-            'location' => '',
+            'company_name' => 'PT. Neoduta Centra Hydraulic Indonesia',
+            'location' => 'Jakarta',
             'company_logo' => 'public/images/neoduta_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Lima Lima Dinamika',
-            'location' => '',
+            'location' => 'Surabaya',
             'company_logo' => 'public/images/LL_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Surya Jaya Muliatama',
-            'location' => '',
+            'location' => 'Jakarta',
             'company_logo' => 'public/images/sjmt_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'CV. Mupakat Aki',
-            'location' => '',
+            'location' => 'Pati',
             'company_logo' => 'public/images/mupakataki_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Qando Qoaching',
-            'location' => '',
+            'location' => 'Jakarta',
             'company_logo' => 'public/images/QQ_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Arindo Garmentama',
-            'location' => '',
+            'location' => 'Semarang',
             'company_logo' => 'public/images/arindo_logo.png',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'Raja Jaya Teknik',
-            'location' => '',
+            'location' => 'Kudus',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
-            'company_name' => 'PT. Sinergi Inti Persada ',
-            'location' => '',
+            'company_name' => 'PT. Sinergi Inti Persada',
+            'location' => 'Surabaya',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
-            'company_name' => 'PT. Wan Bao Long Steel ',
-            'location' => '',
+            'company_name' => 'PT. Wan Bao Long Steel',
+            'location' => 'Bekasi',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Riau Perkasa Steel',
-            'location' => '',
+            'location' => 'Riau',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'Yayasan Karya insan Sejahtera',
-            'location' => '',
+            'location' => 'Jakarta',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'CV. Engoni Tuntang',
-            'location' => '',
+            'location' => 'Salatiga',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'CV. Santosa Jayang',
-            'location' => '',
+            'location' => 'Pati',
             'company_logo' => '',
         ]);
         DB::table("clients")->insert([
             'company_name' => 'PT. Surya Lembah Merkon',
-            'location' => '',
+            'location' => 'Boyolali',
+            'company_logo' => 'public/images/suryalembahmerkon_logo.jpeg',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'CV. Anugrah Karya Mandiri',
+            'location' => 'Semarang',
             'company_logo' => '',
         ]);
+        DB::table("clients")->insert([
+            'company_name' => 'PT. Masa Baru Guna Persada',
+            'location' => 'Kudus',
+            'company_logo' => 'public/images/masabaru_logo.jpeg',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'PT. Energi Yijie Langyu',
+            'location' => 'Tangerang',
+            'company_logo' => '',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'UD Iman Sejati',
+            'location' => 'Semarang',
+            'company_logo' => '',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'The Peak Garage',
+            'location' => 'Semarang',
+            'company_logo' => 'public/images/peakgarage_logo.jpeg',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'PT. Parama Bara Intercontinental',
+            'location' => 'Jakarta',
+            'company_logo' => '',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'CV. Bangun Baru Jaya',
+            'location' => 'Semarang',
+            'company_logo' => 'public/images/bangunbarujaya_logo.jpeg',
+        ]);
+        DB::table("clients")->insert([
+            'company_name' => 'CV. Jati Manis',
+            'location' => 'Semarang',
+            'company_logo' => '',
+        ]);
+
 
     }
 }
